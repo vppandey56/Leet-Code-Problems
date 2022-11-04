74429eaa32b47c9bdc13df83511d0f9e9b8910fb
Ved Prakash Pandey <75631832+vppandey56@users.noreply.github.com>
2022-11-04 16:44:48 +0530

    Update and rename 1- find Peak Element to 01_(find Peak Element).cpp

--- Medium"/01_(find Peak Element).cpp
+++ Medium"/01_(find Peak Element).cpp
@@ -1,4 +1,4 @@
-Example 1:
+/*Example 1:
 
 Input: nums = [1,2,3,1]
 Output: 2
@@ -8,6 +8,7 @@ Example 2:
 Input: nums = [1,2,1,3,5,6,4]
 Output: 5
 Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+*/
 
 
 class Solution {
